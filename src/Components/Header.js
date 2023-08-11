cc738991a0a020ca8260c41dd08390668226926c
diyatandel1312 <diyatandel7462@gmail.com>
2023-08-11 12:08:46 +0530

    my name

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+  
+function Header() {
+    return (
+        <header>
+        <p>This is header.-this rending header.</p>
+       </header>
+    )
+}
+
+export default  Header;
